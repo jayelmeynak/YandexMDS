680392add67b084231c40ae9be420b5f7a2e41a3
jayelmeynak <magomed110301@yandex.ru>
2024-08-14 13:52:59 +0300

    Add param "achievement"

--- app/src/main/java/com/example/yandexmds/domain/model/ToDoItemEntity.kt
+++ app/src/main/java/com/example/yandexmds/domain/model/ToDoItemEntity.kt
@@ -4,5 +4,6 @@ data class ToDoItemEntity(
     val id: Int,
     val description: String,
     val significance: Significance = Significance.USUAL,
+    val achievement: Boolean,
     val deadline: String
 )
