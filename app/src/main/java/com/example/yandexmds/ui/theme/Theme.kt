da8c995bac62c993005e4a6fe7f2e757ea51fb1d
jayelmeynak <magomed110301@yandex.ru>
2024-08-14 10:47:10 +0300

    Added fonts and colors

--- app/src/main/java/com/example/yandexmds/ui/theme/Theme.kt
+++ app/src/main/java/com/example/yandexmds/ui/theme/Theme.kt
@@ -8,18 +8,23 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFF161618),
+    secondary = Color(0xFF252528),
+    onPrimary = White,
+    onSecondary = Color(0x99FFFFFF),
+    onTertiary = Color(0x66FFFFFF),
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xFFF7F6F2),
+    secondary = White,
+    onPrimary = Color(0xFF000000),
+    onSecondary = Color(0x99000000),
+    onTertiary = Color(0x4D000000),
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -36,7 +41,7 @@ private val LightColorScheme = lightColorScheme(
 fun YandexMDSTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
--- app/src/main/java/com/example/yandexmds/ui/theme/Type.kt
+++ app/src/main/java/com/example/yandexmds/ui/theme/Type.kt
@@ -2,33 +2,56 @@ package com.example.yandexmds.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.yandexmds.R
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_black, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.roboto_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.roboto_regular, FontWeight.Normal, FontStyle.Normal),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    bodyMedium = TextStyle(
+        fontFamily = Roboto,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    titleLarge = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Medium,
+        fontSize = 32.sp,
+        lineHeight = 37.5.sp,
+        letterSpacing = 0.7.sp
+    ),
+
+    titleMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        lineHeight = 32.sp
+    ),
+
+    titleSmall = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    ),
+
+    labelMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
-)
+ )
