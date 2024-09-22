c13494e764b2c87436f10f2f9a204398ca941001
jayelmeynak <magomed110301@yandex.ru>
2024-09-22 22:09:29 +0300

    feat(ui): добавлена логика снятия фокуса и скрытия клавиатуры для TextField на экране AddEditScreen
    - Добавлено снятие фокуса с TextField при клике на другие элементы экрана
    - Реализовано автоматическое скрытие клавиатуры при потере фокуса TextField

--- app/src/main/java/com/example/yandexmds/presentation/screens/addAndEdit/AddTaskScreen.kt
+++ app/src/main/java/com/example/yandexmds/presentation/screens/addAndEdit/AddTaskScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,6 +19,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Switch
 import androidx.compose.material.SwitchDefaults
@@ -36,6 +38,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -49,7 +52,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -88,6 +95,9 @@ fun AddScreen(id: Int?, navController: NavController) {
     val pickedDate = remember {
         mutableStateOf<LocalDate?>(null)
     }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
 
     LaunchedEffect(id) {
         if (id != null) {
@@ -213,14 +223,28 @@ fun AddScreen(id: Int?, navController: NavController) {
                     .verticalScroll(scrollState)
                     .fillMaxSize()
                     .padding(horizontal = 16.dp)
+                    .pointerInput(Unit) {
+                        detectTapGestures(
+                            onTap  = {
+                                focusManager.clearFocus()
+                                keyboardController?.show()
+                            }
+                        )
+                    }
             ) {
                 TextField(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(vertical = 8.dp)
-                        .background(color = MaterialTheme.colorScheme.tertiary),
+                        .padding(vertical = 8.dp),
                     minLines = 3,
                     maxLines = 11,
+                    shape = RoundedCornerShape(10.dp),
+                    colors = TextFieldDefaults.colors().copy(
+                        focusedContainerColor = MaterialTheme.colorScheme.tertiary,
+                        unfocusedContainerColor = MaterialTheme.colorScheme.tertiary,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    ),
                     placeholder = {
                         Text(
                             modifier = Modifier.padding(start = 4.dp),
