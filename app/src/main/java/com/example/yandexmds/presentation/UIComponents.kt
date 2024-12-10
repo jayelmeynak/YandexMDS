08c82ab162881d05e443f3bd4cf691067485977b
jayelmeynak <magomed110301@yandex.ru>
2024-12-11 00:14:28 +0300

    feat: создал компонент ScheduleTimePicker для выбора времени начала и окончания

--- app/src/main/java/com/example/yandexmds/presentation/UIComponents.kt
+++ app/src/main/java/com/example/yandexmds/presentation/UIComponents.kt
@@ -1,5 +1,8 @@
 package com.example.yandexmds.presentation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -10,11 +13,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.yandexmds.R
 import com.example.yandexmds.ui.theme.Blue
+import com.vanpra.composematerialdialogs.MaterialDialog
+import com.vanpra.composematerialdialogs.MaterialDialogState
+import com.vanpra.composematerialdialogs.datetime.time.TimePickerDefaults
+import com.vanpra.composematerialdialogs.datetime.time.timepicker
+import java.time.LocalTime
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -47,4 +56,46 @@ fun AddTopBar(
         },
         title = { }
     )
+}
+
+
+
+@Composable
+fun ScheduleTimePicker(
+    isStartTime: Boolean,
+    timeDialogState: MaterialDialogState,
+    pickedTime: MutableState<LocalTime?>,
+) {
+
+    Box(
+        modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer),
+    ) {
+        Button(
+            onClick = { timeDialogState.show() }) {
+            Text(
+                text = pickedTime.value?.toString() ?: if (isStartTime) "Начало" else "Конец",
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
+    }
+    MaterialDialog(
+        dialogState = timeDialogState,
+        buttons = {
+            positiveButton(
+                text = "Готово",
+                textStyle = MaterialTheme.typography.labelMedium.copy(color = Blue)
+            )
+            negativeButton(
+                text = "Отмена",
+                textStyle = MaterialTheme.typography.labelMedium.copy(color = Blue)
+            )
+        }
+    ) {
+        timepicker(
+            initialTime = LocalTime.MIDNIGHT,
+            colors = TimePickerDefaults.colors()
+        ) { time ->
+            pickedTime.value = time
+        }
+    }
 }
